db3a401b3dfa337e1d54d53259c8bb86301fef85
sijokapt <jodelfotso@stud.hawk.de>
2026-07-05 17:28:06 +0200

    feat: add ROI reload and visualization with cv2.rectangle and cv2.putText

--- src/roi_selector.py
+++ src/roi_selector.py
@@ -9,27 +9,49 @@ image = cv2.imread("test_images/test_images/ok/ok_001.png")
 #     print (f"ROI sélectionné → x_{i}={x}, y_{i}={y}, w_{i}={w}, h_{i}={h}")
 #     ROI = image[y:y+h, x:x+w]
 #     cv2.imshow(f"ROI{i}", ROI)
-rois = cv2.selectROIs("Surface Inspection", image)
 
-# 3. Afficher toutes les coordonnées + découper chaque ROI
-rois_data = []
-for i, (x, y, w, h) in enumerate(rois):
-    print(f"ROI {i+1} → x={x}, y={y}, w={w}, h={h}")
-    roi = image[y:y+h, x:x+w]
-    cv2.imshow(f"ROI_{i+1}", roi)
-     # Ajouter à la liste — pas encore écrire
-    rois_data.append({
-        "id":  i + 1,
-        "x":   int(x),
-        "y":   int(y),
-        "w":   int(w),
-        "h":   int(h)
-    })
-# 4. Écrire UNE SEULE FOIS après la boucle
-os.makedirs("results", exist_ok=True)
-with open("results/rois.json", "w") as f:
-    json.dump(rois_data, f, indent=4)
+# rois = cv2.selectROIs("Surface Inspection", image)
 
-print(f"\n✅ ROIs sauvegardées → results/rois.json")
+# # 3. Afficher toutes les coordonnées + découper chaque ROI
+# rois_data = []
+# for i, (x, y, w, h) in enumerate(rois):
+#     print(f"ROI {i+1} → x={x}, y={y}, w={w}, h={h}")
+#     roi = image[y:y+h, x:x+w]
+#     cv2.imshow(f"ROI_{i+1}", roi)
+#      # Ajouter à la liste — pas encore écrire
+#     rois_data.append({
+#         "id":  i + 1,
+#         "x":   int(x),
+#         "y":   int(y),
+#         "w":   int(w),
+#         "h":   int(h)
+#     })
+# # 4. Écrire UNE SEULE FOIS après la boucle
+# os.makedirs("results", exist_ok=True)
+# with open("results/rois.json", "w") as f:
+#     json.dump(rois_data, f, indent=4)
+
+# print(f"\n✅ ROIs sauvegardées → results/rois.json")
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+with open("results/rois.json", "r") as f:
+    data = json.load(f)
+
+for roi in data:
+    x, y, w, h = roi["x"], roi["y"], roi["w"], roi["h"]
+
+    # Rectangle vert
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Numéro du ROI en haut à gauche du rectangle
+    cv2.putText(image, f"ROI {roi['id']}",
+                (x, y - 8),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+
+    print(f"ROI {roi['id']} → x={x}, y={y}, w={w}, h={h}")
+
+cv2.imshow("Surface — ROIs", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
